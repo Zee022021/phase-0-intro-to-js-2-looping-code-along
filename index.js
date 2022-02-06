2cac792e12045ea53db0a48ab0ad768636257f4b
ZiaulhaqHaqyar <fnu.ziaulhaq@flatironschool.com>
2022-02-06 09:46:29 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+/*for (let age = 30; age < 40; age++) {
+    console.log(`I'am ${age} years old. Happy birthday to me`);
+    debugger;
+}*/
+
+/*const gifts = ["panda", "car toy", "tricycle"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+    return gifts;
+}
+
+wrapGifts(gifts);*/
+
+let folks = ["Charlie", "Samip", "Ali"];
+let newArray = [];
+
+function writeCards(folks) {
+    for (let i = 0; i < folks.length; i++) {
+        console.log(`"Thank you ${folks[i]}, for the birthday gift."`)
+    }
+    return folks;
+}
+writeCards(newArray.push(i))
+
+function countDown() {
+    let countDown = 11;
+    while ( countDown > 0) {
+        console.log(countDown--);
+    };
+    return countDown;
+}
+console.log(countDown)
